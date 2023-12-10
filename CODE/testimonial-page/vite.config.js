1cf5881c85830af372d7d95d21b0413f85c88d29
Achal <achaldev3@gmail.com>
2023-12-10 17:43:36 +0530

    deploy:testimonial page

--- CODE/testimonial-page/vite.config.js
+++ CODE/testimonial-page/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://achal-00.github.io/devChallenges/testimonial-page",
+  build: {
+    outDir: "../../testimonial-page",
+  },
+});
