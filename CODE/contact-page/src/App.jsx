9595cafe86ac5ebfc4c26575b18225898b515aec
Achal <achaldev3@gmail.com>
2023-12-26 22:22:07 +0530

    deploy:contact page:-fix bg

--- CODE/contact-page/src/App.jsx
+++ CODE/contact-page/src/App.jsx
@@ -5,7 +5,7 @@ import Form from "./components/Form/Form";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-[url('bg-image.svg')] bg-no-repeat bg-cover p-4 flex flex-col gap-16 font-custom-font">
+    <div className="min-h-screen bg-[url('/bg-image.svg')] bg-no-repeat bg-cover p-4 flex flex-col gap-16 font-custom-font">
       <Navbar />
       <Heading />
       <Form />
